4c1b1382c007d4888b3574c59078c65ce8859ea1
GaichorTech <vasudeogaichor@gmail.com>
2022-01-06 18:26:28 +0530

    issue in deployment

--- vasudeo_portfolio/settings.py
+++ vasudeo_portfolio/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vasudeogaichor.pythonanywhere.com']
+ALLOWED_HOSTS = ['vasudeogaichor.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
