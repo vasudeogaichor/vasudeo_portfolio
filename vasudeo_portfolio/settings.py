1f2a42c31cd329193dc69a05e5e8334af3399abe
GaichorTech <vasudeogaichor@gmail.com>
2022-01-09 18:34:56 +0530

    added a blog

--- vasudeo_portfolio/settings.py
+++ vasudeo_portfolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['13.235.17.120','127.0.0.1']
 
