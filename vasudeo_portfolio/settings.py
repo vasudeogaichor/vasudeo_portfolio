3c0e9e943c7916b1a58e1b808f89d3df7309f42f
Vasudeo Gaichor <76909371+GaichorTech@users.noreply.github.com>
2022-04-06 21:02:59 +0530

    Update settings.py

--- vasudeo_portfolio/settings.py
+++ vasudeo_portfolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['35.154.212.12','vasudeogaichor.pythonanywhere.com']
 
